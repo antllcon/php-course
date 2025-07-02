026c9d66b20ac2667698bd12e8612b843b745de6
antllcon <pokemonivan32@gmail.com>
2025-07-02 04:48:03 +0300

    Тестовый index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$firstName = "Степан";
+$lastName = "Глухарев";
+$birthday = "10-07-2005";
+
+/**
+ * @throws Exception
+ */
+function GetAge($birthday): DateInterval
+{
+    $birthDate = new DateTime($birthday);
+    $currentDate = new DateTime();
+    return $currentDate->diff($birthDate);
+}
+
+try {
+    echo "Привет, меня зовут " . $firstName . " " . $lastName .
+        " и мне " . GetAge($birthday)->y . " лет. " .
+        "Люблю лес, баню, Россию!";
+} catch (Exception $e) {
+    echo "Ошибка в методе получения возраста.";
+}
